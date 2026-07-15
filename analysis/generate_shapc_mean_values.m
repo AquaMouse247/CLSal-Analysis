31679083bdfb112193252f95561f016b7457a482
AquaMouse247 <aquabanyie@gmail.com>
2026-07-15 12:23:33 -0400

    Updated SHAPC-Mean generation script and setup file for PathMNIST.

--- analysis/generate_shapc_mean_values.m
+++ analysis/generate_shapc_mean_values.m
@@ -25,13 +25,12 @@ end
 % Load SHAPC values (First and Last 1000)
 for i=1:length(algs)
     alg = algs(i);
-    if dataset == "cifar10"
-        save_path = sprintf("%s/%s/shapc_vals_first_last_1000.mat", alg, dataset);
-    elseif dataset == "cifar100"
-        save_path = sprintf("%s/%s/shapc_vals_first_last_1000.mat", alg, dataset);
-    
+    if dataset == "cifar100"
+        save_path = sprintf("%s/%s/shapc_vals_first_last_2000.mat", alg, dataset);
     elseif dataset == "imagenet200"
         save_path = sprintf("%s/%s/shapc_vals_first_last_4000.mat", alg, dataset);
+    else
+        save_path = sprintf("%s/%s/shapc_vals_first_last_1000.mat", alg, dataset);
     end
 
     if isfile(save_path)
@@ -109,7 +108,7 @@ disp(sorted_shapc_first_last_1000) %[output:520f5d08]
 %[appendix]{"version":"1.0"}
 %---
 %[metadata:view]
-%   data: {"layout":"onright","rightPanelPercent":37.3}
+%   data: {"layout":"onright","rightPanelPercent":45.7}
 %---
 %[output:520f5d08]
 %   data: {"dataType":"text","outputData":{"text":"              <strong>Accuracy (%)<\/strong>    <strong>SHAPC-Mean (%)<\/strong>    <strong>Time (hrs)<\/strong>\n              <strong>____________<\/strong>    <strong>______________<\/strong>    <strong>__________<\/strong>\n\n    <strong>iTAML <\/strong>         40.1           22.723          17.474  \n    <strong>xder  <\/strong>        40.05           23.432             NaN  \n    <strong>icarl <\/strong>        38.75           24.426          8.6244  \n    <strong>memo  <\/strong>        55.25           28.207          20.867  \n    <strong>foster<\/strong>        54.62           28.409          18.886  \n    <strong>der   <\/strong>        57.38            37.31           52.08  \n    <strong>dsal  <\/strong>       38.248           37.589             NaN  \n    <strong>tagfex<\/strong>       57.939           37.618             NaN  \n    <strong>RPSnet<\/strong>          NaN              NaN             NaN  \n\n","truncated":false}}
--- analysis/setup.m
+++ analysis/setup.m
@@ -6,3 +6,6 @@ dataset_configs = {};
 dataset_configs.cifar10 = struct('num_sessions', 10, 'num_classes', 10, 'cls_per_task', 2, 'samples_per_cls', 100);
 dataset_configs.cifar100 = struct('num_sessions', 10, 'num_classes', 100', 'cls_per_task', 10, 'samples_per_cls', 20);
 dataset_configs.imagenet200 = struct('num_sessions', 10, 'num_classes', 200', 'cls_per_task', 20, 'samples_per_cls', 20);
+
+% MedMNIST Datasets
+dataset_configs.pathmnist = struct('num_sessions', 3, 'num_classes', 9, 'cls_per_task', 3, 'samples_per_cls', 100);
